f00c5823e88c00726cb32fd1ebe2c0e4d35a4410
Lucas Queiroz <lucasestevam365@gmail.com>
2023-10-13 16:02:45 -0300

    Adicionar arquivos de projeto.

---
+++ Interface/Program.cs
@@ -0,0 +1,22 @@
+﻿using Interface.Entities;
+using System.Globalization;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm):");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm):");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter price per hour:");
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            Console.WriteLine("Enter price per day:");
+        }
+    }
+}
---
+++ Interface/Services/RentalService.cs
@@ -0,0 +1,21 @@
+﻿using Interface.Entities;
+
+namespace Interface.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvouce(CarRental carRental)
+        {
+
+        }
+    }
+}
